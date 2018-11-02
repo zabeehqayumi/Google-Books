8a1bc26cf290c32bd48e9db6c0695a270b57acf2
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-11-02 11:27:56 -0700

    Json parsing done.

--- Google Books/ViewController.swift
+++ Google Books/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        jsonFile("harrypotter")
     }
-
-
+    
+    //https://www.googleapis.com/books/v1/volumes?q=harrypotter
+    
+    func jsonFile(_ booksName : String){
+        
+        let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=\(booksName)")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            guard let data = data else {return}
+            
+            do{
+                let json = try JSON(data: data)
+                print(json)
+                
+                
+            }catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
 }
 
