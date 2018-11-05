8545d24f10e191792ad94e2fc34506166e8743da
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-11-04 22:05:27 -0800

    Deleting feature implemented persistently

--- Google Books/ViewControllerTwo.swift
+++ Google Books/ViewControllerTwo.swift
@@ -13,11 +13,12 @@ class ViewControllerTwo: UIViewController {
     
     
     var favorites : [String] = []
-  
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         let defaults = UserDefaults.standard
         if let favoritesDefaults = defaults.object(forKey: "favorites"){
@@ -25,7 +26,22 @@ class ViewControllerTwo: UIViewController {
         }
         
         self.tableView.reloadData()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.tableView.reloadData()
+        
+    }
+    @IBAction func deleteButtonPresed(_ sender: Any) {
+        
+        
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: "favorites")
+        self.tableView.reloadData()
 
+        
+        
     }
     
 }
@@ -39,11 +55,26 @@ extension ViewControllerTwo : UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        
-       // cell.textLabel?.text = favBooks[indexPath.row].title
         cell.textLabel?.text = favorites[indexPath.row]
         
         return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            self.favorites.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+        }
+        
+        
+        
     }
     
     
